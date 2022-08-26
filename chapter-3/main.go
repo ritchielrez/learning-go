8a0ffab5a2f321f2772136f14efdab3c2ad6d98d
RitchielRez <ritchielrez@gmail.com>
2022-08-26 11:54:50 +0600

    feat(chapter-3): fixed a grammertical mistake

--- chapter-3/main.go
+++ chapter-3/main.go
@@ -14,7 +14,7 @@ func main() {
 	// `cmd.Output()` returns the output as a bytes array
 	// strings library can be used for string conversion
 	// as the bytes array contains the ascii value of each
-	// characters
+	// character
 	cmdOutput := strings.TrimSuffix(string(out), "\n")
 	// cmdOutput := string(out)
 	fmt.Println([]byte(cmdOutput))
@@ -22,7 +22,7 @@ func main() {
 	testStr := "Test String \n"
 	testBytes := []byte(testStr)
 	fmt.Printf("\n%v", testBytes)
-	
+
 	// The suffix(ending chars) would be removed from
 	// the 1st arg provided to the `strings.TrimSuffix()`
 	testStrTrim := strings.TrimSuffix(testStr, "\n")
