367db4c7c8ab79d6ea35e796f3096895772b9158
RitchielRez <ritchielrez@gmail.com>
2022-08-30 20:40:00 +0600

    feat(chapter-4): experimentation with arrays

--- chapter-4/main.go
+++ chapter-4/main.go
@@ -3,9 +3,20 @@ package main
 import "fmt"
 
 func main() {
-    arr_str := [2]string{"Hello", "World!"}
+	arr_str := [2]string{"Hello", "World!"}
 
-    for index, value := range arr_str {
-        fmt.Printf("Index: %v, Value: %v\n", index, value)
-    }
+	for index, value := range arr_str {
+		fmt.Printf("Index: %v, Value: %v\n", index, value)
+	}
+
+	cashbacks := []int{1000, 2000, 3000, 4000}
+	biggest_cashback := cashbacks[0]
+
+	for _, cashback := range cashbacks {
+		if cashback > biggest_cashback {
+			biggest_cashback = cashback
+		}
+	}
+
+	fmt.Printf("\nBiggest cashback avalaible is %v dollars\n", biggest_cashback)
 }
