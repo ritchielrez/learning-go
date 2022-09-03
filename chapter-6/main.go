d03ba843f246b22d7986dbd635b9d5ce2dec735f
RitchielRez <ritchielrez@gmail.com>
2022-09-03 16:30:33 +0600

    feat(chapter-6): updates

--- chapter-6/main.go
+++ chapter-6/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 )
 
-func average(nums []float64) float64 {
+// Variadic functions can take endless numbers
+// of arguments of specific types
+func average(nums ...float64) float64 {
 	// Panicking is a way to interrupt a running proccess
 	// like a specific function by raising a exception runtime
 	// panic("Testing")
@@ -30,10 +32,10 @@ func biggestSalary(salaries []int) int {
 
 func main() {
 	var numbers = []float64{20, 10, 30}
-
-	fmt.Printf("Average: %v\n", average(numbers))
+	// use ... to pass all elements as individual
+	fmt.Printf("Average: %v\n", average(numbers...))
 
 	var salaries = []int{1000, 2500, 5000, 7500, 10000}
-
+	salaries = append(salaries, 15000)
 	fmt.Printf("Biggest salary available: %v\n", biggestSalary(salaries))
 }
