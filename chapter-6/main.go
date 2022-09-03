7ec17986a21c9d27336cec323f8cc122a7e18ef2
RitchielRez <ritchielrez@gmail.com>
2022-09-03 13:51:52 +0600

    feat(chapter-6): added one more function

--- chapter-6/main.go
+++ chapter-6/main.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func average(nums []float64) float64 {
+	// Panicking is a way to interrupt a running proccess
+	// like a specific function by raising a exception runtime
+	// panic("Testing")
+
 	total := 0.0
 	for _, num := range nums {
 		total += num
@@ -10,8 +16,24 @@ func average(nums []float64) float64 {
 	return total / float64(len(nums))
 }
 
+func biggestSalary(salaries []int) int {
+	biggestSalary := salaries[0]
+
+	for _, salary := range salaries {
+		if salary > biggestSalary {
+			biggestSalary = salary
+		}
+	}
+
+	return biggestSalary
+}
+
 func main() {
 	var numbers = []float64{20, 10, 30}
 
 	fmt.Printf("Average: %v\n", average(numbers))
+
+	var salaries = []int{1000, 2500, 5000, 7500, 10000}
+
+	fmt.Printf("Biggest salary available: %v\n", biggestSalary(salaries))
 }
