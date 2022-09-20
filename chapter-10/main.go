bcd07198e9a537173fc48d00339f90d5f5462b66
RitchielRez <ritchielrez@gmail.com>
2022-09-20 20:44:17 +0600

    feat(structs): fucntion for detecting volume of a rectangle

--- chapter-10/main.go
+++ chapter-10/main.go
@@ -5,16 +5,19 @@ import "fmt"
 type Rectangle struct {
 	xCoordinate float64
 	yCoordinate float64
+  zCoordinate float64
 }
 
 func RectangleArea(rectangle Rectangle) float64 {
 	return rectangle.xCoordinate * rectangle.yCoordinate
 }
 
-func main() {
-	rect1 := Rectangle{10, 20}
-	fmt.Printf("Area of rectangle 1: %v\n", RectangleArea(rect1))
+func RectangleVolume(rectangle Rectangle) float64 {
+  return rectangle.xCoordinate * rectangle.yCoordinate * rectangle.zCoordinate
+}
 
-	rect2 := Rectangle{10, 20}
-	fmt.Printf("Area of rectangle 1: %v\n", RectangleArea(rect2))
+func main() {
+	rect1 := Rectangle{10, 20, 30}
+	fmt.Printf("Area of rectangle 1: %v\n", RectangleArea(rect1))
+	fmt.Printf("Volume of rectangle 1: %v\n", RectangleVolume(rect1))
 }
