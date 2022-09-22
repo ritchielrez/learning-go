9696d2c39de26c2fbd68cb8ff2733322e4e6038e
RitchielRez <ritchielrez@gmail.com>
2022-09-22 21:13:04 +0600

    feat(structs): inheritence, in-struct methods

--- chapter-10/main.go
+++ chapter-10/main.go
@@ -12,12 +12,32 @@ func RectangleArea(rectangle Rectangle) float64 {
 	return rectangle.xCoordinate * rectangle.yCoordinate
 }
 
-func RectangleVolume(rectangle Rectangle) float64 {
+// Let's define a method, which lives outside. But is a in-struct
+// or-object method. Just defined outside.
+func (rect *Rectangle) RectangleVolume(rectangle Rectangle) float64 {
 	return rectangle.xCoordinate * rectangle.yCoordinate * rectangle.zCoordinate
 }
 
+// Inheriting from `Rectangle` struct/object
+type Square struct {
+  Rectangle // An anonymous field
+}
+
+func (sqr *Square) SquareArea(square Square) float64 {
+  return square.xCoordinate * square.xCoordinate
+}
+
+func (sqr *Square) SquareVolume(square Square) float64 {
+  return square.xCoordinate * square.xCoordinate * square.xCoordinate
+}
 func main() {
 	rect1 := Rectangle{10, 20, 30}
 	fmt.Printf("Area of rectangle 1: %v\n", RectangleArea(rect1))
-	fmt.Printf("Volume of rectangle 1: %v\n", RectangleVolume(rect1))
+	fmt.Printf("Volume of rectangle 1: %v\n", rect1.RectangleVolume(rect1))
+  fmt.Println()
+
+  sqr1 := Square{}
+  sqr1.xCoordinate = 10
+  fmt.Printf("Area of square 1: %v\n", sqr1.SquareArea(sqr1))
+  fmt.Printf("Volume of square 1: %v\n", sqr1.SquareVolume(sqr1))
 }
