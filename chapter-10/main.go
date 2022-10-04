339231f77e76359df8ea61d8711d38426830b622
RitchielRez <ritchielrez@gmail.com>
2022-10-04 15:39:59 +0600

    feat(structs): nested structs

--- chapter-10/main.go
+++ chapter-10/main.go
@@ -27,12 +27,25 @@ type Rectangle struct {
 	zCoordinate float64
 }
 
+type MultiShape struct {
+	shapes []Shape
+}
+
 func (Rectangle *Rectangle) area() float64 {
 	return Rectangle.xCoordinate * Rectangle.yCoordinate
 }
 
 func (Rectangle *Rectangle) volume() float64 {
 	return Rectangle.xCoordinate * Rectangle.yCoordinate * Rectangle.zCoordinate
+}
+
+func (MultiShape *MultiShape) volume() float64 {
+	var totalVol float64
+
+	for _, shape := range MultiShape.shapes {
+		totalVol += shape.volume()
+	}
+	return totalVol
 }
 
 func totalVolume(shapes ...Shape) float64 {
@@ -61,4 +74,22 @@ func main() {
 	fmt.Println()
 
 	fmt.Printf("Total volume of both shapes is: %v\n", totalVolume(&sqr, &rect))
+
+	multiShape := MultiShape{
+		shapes: []Shape{
+			&Square{10},
+
+			/* `xCoordinate` is defined in the `Square` struct, so we have set it this way
+			   `Square{10}` is a compisite literal, here because 1st field of the `Rectangle` struct is type of a `Square`,
+			   this literal will create a Square struct, set `xCoordinate = 10`. Because `Rectangle` inherits from `Square`,
+			   `xCoordinate` gets shared. */
+			&Rectangle{
+				Square{10},
+				20,
+				30,
+			},
+		},
+	}
+
+	fmt.Printf("Total volume of multiple shapes is: %v\n", multiShape.volume())
 }
